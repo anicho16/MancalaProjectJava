7542545e02930d5cf8f69d4ea3414fa921fa2103
Amber Nicholas <anicho16@my.westga.edu>
2022-06-18 10:53:02 -0500

    Created FarStrategy Class

---
+++ src/edu/westga/cs6910/mancala/model/strategies/FarStrategy.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+package edu.westga.cs6910.mancala.model.strategies;
+
+/**
+ * Contains algorithm for the computer player to select the pit farthest from its goal
+ * 
+ * @author Amber Nicholas
+ * @version 6.18.22
+ *
+ */
+public class FarStrategy implements SelectStrategy {
+	/**
+	 * Always chooses the pit that is farthest from the goal that contains stones
+	 * @param pits - array of pits that may contain stones
+	 * @return selectedPit - pit farthest the goal with stones
+	 */
+	public int selectPit(int[] pits) {
+		int selectedPit = pits.length / 2;
+		
+		for (int iterator = pits.length - 1; iterator > pits.length / 2; iterator--) {
+			if (pits[iterator] != 0) {
+				selectedPit = iterator;
+			}
+		}
+		return selectedPit;
+	}
+
+}
