103895c57f3794633cc9f4ec7f792dbe83f0c997
Amber Nicholas <anicho16@my.westga.edu>
2022-06-18 12:32:32 -0500

    Adjusted JUnit testing for NearStrategy and FarStrategy classes

--- src/edu/westga/cs6910/mancala/test/strategies/TestChooseFarStrategy.java
+++ src/edu/westga/cs6910/mancala/test/strategies/TestChooseFarStrategy.java
@@ -11,36 +11,34 @@ import org.junit.jupiter.api.Test;
 import edu.westga.cs6910.mancala.model.ComputerPlayer;
 import edu.westga.cs6910.mancala.model.Game;
 import edu.westga.cs6910.mancala.model.strategies.FarStrategy;
-import edu.westga.cs6910.mancala.model.strategies.NearStrategy;
 
 /**
- * Test NearStrategy class causes the computer to select the pit nearest to its goal.
+ * Test NearStrategy class causes the computer to select the pit farthest from its goal.
  * 
  * @author Amber Nicholas
  * @version 6.11.22
  *
  */
-class TestChooseNearStrategy {
+class TestChooseFarStrategy {
 
 	private Game newGame = new Game();
 
 	private ComputerPlayer computerPlayer1 = new ComputerPlayer(this.newGame);
 
 	/**
-	 * Tests Computer's chooses a pit with stones inside on its side of the board which is nearest to its goal.
-	 * After computer takes first turn, pit 6 has no stones and pit 5 should be
-	 * chosen as it would then be nearest to the computer's goal.
+	 * Tests Computer's chooses a pit with stones inside on its side of the board which is farthest from the goal.
+	 * After computer takes its turn twice, pit 6 should be the only pit left with stones.
 	 */
 	@Test
-	void testChoosePitClosestToGoal() {
+	void testChoosePitFarthesetFromGoal() {
+		this.newGame.getComputerPlayer().setStrategy(new FarStrategy());
 		this.newGame.startNewGame(this.computerPlayer1);
-		this.newGame.getComputerPlayer().setStrategy(new NearStrategy());
 		this.newGame.play(this.computerPlayer1.selectPit());
 		this.newGame.play(this.computerPlayer1.selectPit());
 
 		int result = this.computerPlayer1.selectPit();
 
-		assertEquals(4, result);
+		assertEquals(6, result);
 	}
 
 }
